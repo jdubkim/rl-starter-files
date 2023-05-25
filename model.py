9fd637aa416b66f6041c4bb257ecd83b41d624e0
jdubkim <jeewoo1998@gmail.com>
2023-05-25 12:00:06 +0100

    Fix model to take (64,64,3) image instead of (7,7,3) grid representations.

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ def init_params(m):
 
 
 class ACModel(nn.Module, torch_ac.RecurrentACModel):
-    def __init__(self, obs_space, action_space, use_memory=False, use_text=False):
+    def __init__(self, obs_space, action_space, use_memory=True, use_text=False):
         super().__init__()
 
         # Decide which components are enabled
@@ -24,26 +24,19 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
         self.use_memory = use_memory
 
         # Define image embedding
-        # self.image_conv = nn.Sequential(
-        #     nn.Conv2d(3, 16, (2, 2)),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d((2, 2)),
-        #     nn.Conv2d(16, 32, (2, 2)),
-        #     nn.ReLU(),
-        #     nn.Conv2d(32, 64, (2, 2)),
-        #     nn.ReLU()
-        # )
         self.image_conv = nn.Sequential(
-            nn.Conv2d(3, 16, (3, 3)),
-            nn.ReLU(),
-            nn.Conv2d(16, 32, (3, 3)),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, (3, 3)),
-            nn.ReLU(),
+            nn.Conv2d(3, 32, (4, 4), stride=(2, 2)),
+            nn.ELU(),
+            nn.Conv2d(32, 64, (4, 4), stride=(2, 2)),
+            nn.ELU(),
+            nn.Conv2d(64, 128, (4, 4), stride=(2, 2)),
+            nn.ELU(),
+            nn.Conv2d(128, 256, (4, 4), stride=(2, 2)),
+            nn.ELU(),
         )
         n = obs_space["image"][0]
         m = obs_space["image"][1]
-        self.image_embedding_size = ((n-1)//2-2)*((m-1)//2-2)*64
+        self.image_embedding_size = 4*4*256 
 
         # Define memory
         if self.use_memory:
@@ -63,16 +56,16 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
 
         # Define actor's model
         self.actor = nn.Sequential(
-            nn.Linear(self.embedding_size, 64),
+            nn.Linear(self.embedding_size, 256),
             nn.Tanh(),
-            nn.Linear(64, action_space.n)
+            nn.Linear(256, action_space.n)
         )
 
         # Define critic's model
         self.critic = nn.Sequential(
-            nn.Linear(self.embedding_size, 64),
+            nn.Linear(self.embedding_size, 256),
             nn.Tanh(),
-            nn.Linear(64, 1)
+            nn.Linear(256, 1)
         )
 
         # Initialize parameters correctly
